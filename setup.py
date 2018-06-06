9f3e87db3879d00d84d1d6c7758d10b35eb2a77d
zeyangzhang17 <32386566+zeyangzhang17@users.noreply.github.com>
2018-06-06 14:33:55 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,18 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
+from setuptools import setup
+
+setup(name='explp',
+      version='0.0.1',
+      description='Generating Explanations of Linear Optimisation Solutions',
+      url='https://github.com/zeyangzhang17/explp',
+      author='Zeyang ZHANG',
+      author_email='zeyang.zhang@qq.com',
+      license='MIT',
+      packages=['explp'],
+      zip_safe=False)
+
