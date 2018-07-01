792f210a3ef6279d5ff7191f7899cc93839da0ec
zeyangzhang17 <32386566+zeyangzhang17@users.noreply.github.com>
2018-07-01 20:14:24 +0100

    Delete integer_constraint.py

--- integer_constraint.py
+++
@@ -1,7 +0,0 @@
-
-def integer_constraint(Integer_Variable_Name = []):
-    
-    if set(Integer_Variable_Name).issubset(obj_names) == False:
-        print('Error! The Integer Variable Name DOES NOT Match with Variable Name in Objectives')
-    
-    return Integer_Variable_Name 
