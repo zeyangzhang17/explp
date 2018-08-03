afaf1b8d01bc4c5e9d7bb933d02ea68049e742f0
zeyangzhang17 <32386566+zeyangzhang17@users.noreply.github.com>
2018-08-03 16:44:03 +0100

    Update alternative.py

--- explp/alternative.py
+++ explp/alternative.py
@@ -8,12 +8,17 @@
     # alternative.compare()
 
     
-# Last Updated: 31st July 2018
+# Last Updated: 3rd August 2018
 
 
 
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import copy
+import math
+
+import explp.set
 from explp import solve
 from explp import sensitivity_analysis
 
@@ -21,30 +26,37 @@ from explp import sensitivity_analysis
 # Objective Function:
 
 def objective(Objective_Name=[], Variable_Name=[], Variable_Coefficient=[], Maximise=True):
-
+    
+    explp.set.objective(Objective_Name, Objective_Name, Variable_Coefficient, Maximise)
 
 
 # Multi_objective Function:
 
 def multi_objective(Objective_Name=[], Lambda=[], Variable_Name=[], Variable_Coefficient=[], Maximise=True):
 
-
+    explp.set.multi_objective(Objective_Name, Lambda, Variable_Name, Variable_Coefficient, Maximise)
+    
 
 # Constraint Function:
 
 def constraint(Constraint_Name=[], Constraint_Coefficient=[], Bound_Name=[], Bound_Value=[], Maximise=False, Type=[]):
 
-
+    explp.set.constraint(Constraint_Name, Constraint_Coefficient, Bound_Name, Bound_Value, Maximise, Type)
 
 # Integer_constraint Function:
 
 def integer_constraint(Integer_Variable_Names = []):
     
+    explp.set.integer_constraint(Integer_Variable_Names)
     
 
 # Compare Function:
 
 def compare():
+    
+    # firstly retrieve the deep copy of original inputs and solutions
+    
+    
     
 
 
